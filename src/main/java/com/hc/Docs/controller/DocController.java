bb84cddf1859ca9f4e609d8273555e63b95a575e
Hugo E. Contreras <ehugo209@gmail.com>
2025-11-03 20:12:39 -0300

    alteracao do end-point

--- src/main/java/com/hc/Docs/controller/DocController.java
+++ src/main/java/com/hc/Docs/controller/DocController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/docs")
+@RequestMapping("/api/docs")
 @CrossOrigin("*")
 public class DocController {
     @Autowired
