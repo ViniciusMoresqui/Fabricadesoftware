7b393bc07a2c5a1d7b1b178e425f70f6a4c44f5f
José Rafael <130617673+zrpires@users.noreply.github.com>
2025-11-07 21:50:38 -0300

    Att integracaoFeBeBd/Be directory

--- integracaoFeBeBd/Be/src/main/java/com/hc/Docs/repository/DocRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hc.Docs.repository;
-
-import com.hc.Docs.model.DocModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocRepository extends JpaRepository<DocModel, Long> {
-}
