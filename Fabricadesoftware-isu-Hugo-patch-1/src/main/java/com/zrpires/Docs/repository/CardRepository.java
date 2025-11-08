e8e590be478722677046e0444406664ac0135b37
José Rafael <130617673+zrpires@users.noreply.github.com>
2025-11-07 21:52:09 -0300

    Delete Fabricadesoftware-isu-Hugo-patch-1 directory

--- Fabricadesoftware-isu-Hugo-patch-1/src/main/java/com/zrpires/Docs/repository/CardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zrpires.Docs.repository;
-
-import com.zrpires.Docs.model.CardModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CardRepository extends JpaRepository<CardModel, Long> {
-}
--- Fabricadesoftware-isu-Hugo-patch-1/src/main/java/com/zrpires/Docs/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zrpires.Docs.repository;
-
-import com.zrpires.Docs.model.FileModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<FileModel, Long> {
-}
