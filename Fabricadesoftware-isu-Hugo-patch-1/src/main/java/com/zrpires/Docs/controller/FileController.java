9d2fa2bbb84e0a3dee5670e2abe1aa7d1d8216ee
José Rafael <130617673+zrpires@users.noreply.github.com>
2025-11-07 21:51:45 -0300

    Att Backend 07

---
+++ Fabricadesoftware-isu-Hugo-patch-1/src/main/java/com/zrpires/Docs/controller/FileController.java
@@ -0,0 +1,55 @@
+package com.zrpires.Docs.controller;
+
+
+import com.zrpires.Docs.model.CardModel;
+import com.zrpires.Docs.model.FileModel;
+import com.zrpires.Docs.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/file")
+//@CrossOrigin("*")
+@CrossOrigin(origins = {"http://localhost:3000", "http://127.0.0.1:3000", "*"})
+public class FileController {
+
+    @Autowired
+    FileService fileService;
+
+    @PostMapping("/{id}")
+    public ResponseEntity<CardModel> uploadFile(@PathVariable("id") Long idCard, @RequestParam("file") MultipartFile file){
+        CardModel response = fileService.saveFile(idCard, file);
+        if (response != null){
+            return ResponseEntity.ok(response);
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable("id")Long idFile){
+        FileModel fileModel = fileService.getFileModel(idFile);
+        if (fileModel==null) return ResponseEntity.notFound().build();
+        Resource fileResponse = this.fileService.getFile(idFile);
+        if (fileResponse==null) return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + fileModel.getOriginalFileName() + "\"")
+                .body(fileResponse);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteFile(@PathVariable("id") Long id){
+        if (this.fileService.deleteFile(id)){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
---
+++ Fabricadesoftware-isu-Hugo-patch-1/src/main/java/com/zrpires/Docs/service/FileService.java
@@ -0,0 +1,122 @@
+package com.zrpires.Docs.service;
+
+import com.zrpires.Docs.configuration.FileConfiguration;
+import com.zrpires.Docs.model.CardModel;
+import com.zrpires.Docs.model.FileModel;
+import com.zrpires.Docs.repository.CardRepository;
+import com.zrpires.Docs.repository.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class FileService {
+
+    @Autowired
+    private CardRepository cardRepository;
+    @Autowired
+    private FileRepository fileRepository;
+    @Autowired
+    private FileConfiguration fileConfiguration;
+
+    public FileModel getFileModel(Long id){
+        return this.fileRepository.findById(id).orElse(null);
+    }
+
+    public CardModel saveFile(Long idCard, MultipartFile file){
+        if (!cardRepository.existsById(idCard)){
+            return null;
+        }
+        CardModel card = cardRepository.findById(idCard).get();
+
+        String originalName = file.getOriginalFilename();
+        String ext = originalName.substring(originalName.lastIndexOf("."));
+        String localName = UUID.randomUUID() + ext;
+
+        Path path = Paths.get(fileConfiguration.getName())
+                .resolve(localName);
+        try{
+            file.transferTo(path);
+        }catch (IOException er){
+            return null;
+        }
+
+        FileModel fileReference = new FileModel(originalName, ext, localName);
+        card.addFile(fileReference);
+        return cardRepository.save(card);
+    }
+
+    public Resource getFile(Long fileId){
+        FileModel fileReference = fileRepository.findById(fileId).orElseThrow();
+
+        Path path = Paths.get(fileConfiguration.getName())
+                .resolve(fileReference.getLocalName());
+
+        try {
+             Resource resource = new UrlResource(path.toUri());
+             if (!resource.exists() || !resource.isReadable()){
+                 return null;
+             }
+             return resource;
+        }catch (MalformedURLException er){
+            return null;
+        }
+
+    }
+
+    public boolean deleteFile(Long idFile){
+        Optional<FileModel> fileOpt = this.fileRepository.findById(idFile);
+
+        if(fileOpt.isEmpty()){
+            return false;
+        }
+
+        FileModel file = fileOpt.get();
+
+        Path path = Paths.get(fileConfiguration.getName()).resolve(file.getLocalName())
+                .toAbsolutePath();
+
+        try{
+            System.out.println("existe: "+Files.exists(path)+ " No caminho "+ path);
+            Files.deleteIfExists(path);
+            CardModel card = file.getCard();
+            if (card!=null){
+                card.removeFile(file);
+                cardRepository.save(card);
+            }
+            fileRepository.delete(file);
+            return true;
+        }catch (IOException er){
+            return false;
+        }
+
+    }
+
+    public boolean removeFileListFromStorage(List<FileModel> files){
+        for (FileModel file: files){
+
+            Path path = Paths.get(fileConfiguration.getName()).resolve(file.getLocalName())
+                    .toAbsolutePath();
+            try{
+                System.out.println("existe: "+Files.exists(path)+ " No caminho "+ path);
+                Files.deleteIfExists(path);
+                this.fileRepository.delete(file);
+            }catch (IOException er){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
